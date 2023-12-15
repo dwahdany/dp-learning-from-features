6f12df9139529209ac909859ec6b71cfe90dac88
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2023-12-15 10:34:59 +0000

    feat: ls classification

--- src/dp_learning_ff/__init__.py
+++ src/dp_learning_ff/__init__.py
@@ -1,4 +1,4 @@
-from .least_squares import dp_least_squares
+from .least_squares import dp_least_squares, least_squares_classification
 from .prototypes import give_private_prototypes
 
-__all__ = ["dp_least_squares", "give_private_prototypes"]
+__all__ = ["dp_least_squares", "least_squares_classification", "give_private_prototypes"]
