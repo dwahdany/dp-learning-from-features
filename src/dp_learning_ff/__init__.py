dbf31f49c84f8ad0ed260e2279ac7345ca1751ad
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2024-01-03 11:42:18 +0100

    fix: export LS classifier

--- src/dp_learning_ff/__init__.py
+++ src/dp_learning_ff/__init__.py
@@ -1,11 +1,10 @@
-from .least_squares import dp_least_squares, least_squares_classification
+from .least_squares import LeastSquaresClassifier
 from .prototypes import give_private_prototypes
 from . import mechanisms
 from . import coinpress
 
 __all__ = [
-    "dp_least_squares",
-    "least_squares_classification",
+    "LeastSquaresClassifier",
     "give_private_prototypes",
     "coinpress",
     "mechanisms",
