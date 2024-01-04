e56da21adb5de7936129d5597507fb5951e2e982
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2024-01-04 22:08:52 +0100

    refactor: binary search utils

--- src/dp_learning_ff/utils.py
+++ src/dp_learning_ff/utils.py
@@ -30,3 +30,64 @@ def dp_covariance(
     # Add Gaussian noise to the matrix
     cov += rng.normal(scale=noise_std, size=(d, d))
     return cov
+
+
+def binary_optimize(
+    f,
+    target: float,
+    abs_tol: float = 1e-5,
+    rel_tol: float = 1e-2,
+    min_param: float = 0,
+    min_open: bool = True,
+    max_param: float = np.inf,
+    max_open: bool = True,
+    verbose: bool = False,
+    initial: float = 1,
+    initial_step: float = 0.5,
+    steps: int = 1000,
+):
+    x = initial
+    step = initial_step
+    over = f(x) > target
+
+    def max_comp(x, max_param, max_open):
+        if max_open:
+            return x > max_param
+        else:
+            return x >= max_param
+
+    def min_comp(x, min_param, min_open):
+        if min_open:
+            return x < min_param
+        else:
+            return x <= min_param
+
+    it = 0
+    while True:
+        if verbose:
+            print(f"obj({x}) = {f(x)}")
+        curr_obj = f(x)
+        if (
+            curr_obj < target
+            and target - curr_obj < abs_tol
+            and (target - curr_obj) / target < rel_tol
+        ):
+            break
+        if curr_obj > target:
+            if not over:
+                step /= 2
+                over = True
+            while min_comp(x - step, min_param, min_open):
+                step /= 2
+            x -= step
+        else:
+            if over:
+                step /= 2
+                over = False
+            while max_comp(x + step, max_param, max_open):
+                step /= 2
+            x += step
+        it += 1
+        if it > steps:
+            raise RuntimeError("binary_optimize did not converge")
+    return x
