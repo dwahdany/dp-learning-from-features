6f72692df202cc16ecbabc1f309a0b533acb50be
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2024-01-06 18:11:55 +0000

    fix: export coinpress

--- src/dp_learning_ff/__init__.py
+++ src/dp_learning_ff/__init__.py
@@ -1,11 +1,12 @@
 from . import ext, mechanisms
 from .least_squares import LeastSquaresClassifier
 from .linear_probing import LinearProbingClassifier
+from .mean_estimation.coinpress import CoinpressPrototyping
 
 __all__ = [
     "LeastSquaresClassifier",
     "LinearProbingClassifier",
-    "give_private_prototypes",
+    "CoinpressPrototyping",
     "ext",
     "mechanisms",
 ]
